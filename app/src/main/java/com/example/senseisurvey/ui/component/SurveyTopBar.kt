10eeaea79aed8c5b9067bc50b15c7998803347ac
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2023-11-29 13:15:08 +0300

    survey - TopAppBar

---
+++ app/src/main/java/com/example/senseisurvey/ui/component/SurveyTopBar.kt
@@ -0,0 +1,66 @@
+package com.example.senseisurvey.ui.component
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProgressIndicatorDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.senseisurvey.R
+import com.example.senseisurvey.ui.theme.stronglyDeemphasizedAlpha
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SurveyTopBar(
+    questionIndex: Int,
+    totalQuestionsCount: Int,
+    onClosePressed: () -> Unit
+) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        CenterAlignedTopAppBar(title = {
+            TopBarTitle(
+                questionIndex = questionIndex,
+                totalQuestionsCount = totalQuestionsCount
+            )
+        },
+            actions = {
+                IconButton(modifier = Modifier.padding(4.dp), onClick = onClosePressed) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = stringResource(id = R.string.close),
+                        tint = MaterialTheme.colorScheme.onSurface.copy(stronglyDeemphasizedAlpha)
+                    )
+                }
+            }
+        )
+        val animatedProgress by animateFloatAsState(
+            targetValue = (questionIndex + 1) / totalQuestionsCount.toFloat(),
+            animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec,
+            label = "at ${(questionIndex + 1) / totalQuestionsCount.toFloat()}"
+        )
+        LinearProgressIndicator(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            trackColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f),
+            progress = animatedProgress,
+        )
+    }
+}
+
+@Composable
+fun TopBarTitle(modifier: Modifier = Modifier, questionIndex: Int, totalQuestionsCount: Int) {
+    TODO("Not yet implemented")
+}
